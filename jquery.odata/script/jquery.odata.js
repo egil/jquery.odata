e28162cc5e947f433f4ad2ce80dae13aa0d3a398
Egil Hansen <egil@assimilated.dk>
2010-04-29 09:42:28 +0200

    Basic Odata object created, jQuery extended with new $.odata function.

---
+++ jquery.odata/script/jquery.odata.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+/// <reference path="jquery-1.4.2.js" />
+
+(function ($) {
+    // HACK: remove before publish. Enables VS2010 js intellisense
+    $ = jQuery;
+
+    $.odata = function (serviceRootURI) {
+        ///	<summary>
+        ///		Create a new OData object that can be used to query against
+        ///     the specified service root URI.
+        ///	</summary>
+        ///	<returns type="OData" />
+        ///	<param name="serviceRootURI" type="String">
+        ///		The service root URI of a OData service.
+        ///	</param>
+    };
+    
+}(jQuery));
+
